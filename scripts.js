55465ab2a9b19f1b00fa09272fe849cebf5c47f4
Jeya Suriya <93221485+CJD-haw@users.noreply.github.com>
2022-10-27 01:44:13 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,11 @@ function myFunction() {
     if (x.className === "topnav") { x.className += " responsive"; }
     else { x.className = "topnav"; }
 }
+
+function activation(idx) {
+    for (let i=1;i<=5;i++) {
+        var x = document.getElementById("i"+i);
+        if (i!=idx) { x.className = "inactive"; }
+        else { x.className = "active"; }
+    }
+}
