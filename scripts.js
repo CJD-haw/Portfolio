4bc58a532de15a193473c2e384ebac812db05238
Jeya Suriya <93221485+CJD-haw@users.noreply.github.com>
2022-11-05 22:23:10 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,80 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") { x.className += " responsive"; }
-    else { x.className = "topnav"; }
+
+function showProfile() {
+    document.getElementById("profileShow").style.display = "none";
+    document.getElementById("profileHide").style.display = "block";
 }
 
-function activation(idx) {
-    for (let i=1;i<=5;i++) {
-        var x = document.getElementById("i"+i);
-        if (i!=idx) { x.className = "inactive"; }
-        else { x.className = "active"; }
-    }
+function hideProfile() {
+    document.getElementById("profileShow").style.display = "block";
+    document.getElementById("profileHide").style.display = "none";
+}
+
+function showObjective() {
+    document.getElementById("objectiveShow").style.display = "none";
+    document.getElementById("objectiveHide").style.display = "block";
+}
+
+function hideObjective() {
+    document.getElementById("objectiveShow").style.display = "block";
+    document.getElementById("objectiveHide").style.display = "none";
+}
+
+function showSkill() {
+    document.getElementById("skillShow").style.display = "none";
+    document.getElementById("skillHide").style.display = "block";
+}
+
+function hideSkill() {
+    document.getElementById("skillShow").style.display = "block";
+    document.getElementById("skillHide").style.display = "none";
+}
+
+function showEducation() {
+    document.getElementById("educationShow").style.display = "none";
+    document.getElementById("educationHide").style.display = "block";
+}
+
+function hideEducation() {
+    document.getElementById("educationShow").style.display = "block";
+    document.getElementById("educationHide").style.display = "none";
+}
+
+function showCompetition() {
+    document.getElementById("competitionShow").style.display = "none";
+    document.getElementById("competitionHide").style.display = "block";
+}
+
+function hideCompetition() {
+    document.getElementById("competitionShow").style.display = "block";
+    document.getElementById("competitionHide").style.display = "none";
+}
+
+function showAchievement() {
+    document.getElementById("achievementShow").style.display = "none";
+    document.getElementById("achievementHide").style.display = "block";
+}
+
+function hideAchievement() {
+    document.getElementById("achievementShow").style.display = "block";
+    document.getElementById("achievementHide").style.display = "none";
+}
+
+function showProject() {
+    document.getElementById("projectShow").style.display = "none";
+    document.getElementById("projectHide").style.display = "block";
+}
+
+function hideProject() {
+    document.getElementById("projectShow").style.display = "block";
+    document.getElementById("projectHide").style.display = "none";
+}
+
+function showOrganization() {
+    document.getElementById("organizationShow").style.display = "none";
+    document.getElementById("organizationHide").style.display = "block";
+}
+
+function hideOrganization() {
+    document.getElementById("organizationShow").style.display = "block";
+    document.getElementById("organizationHide").style.display = "none";
 }
